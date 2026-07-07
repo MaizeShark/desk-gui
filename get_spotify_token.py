234d1ea50603d8869d8edd0d8c6693a5727baea3
Niclas Eisenhut <niclaseisenhut@gmail.com>
2026-07-07 19:51:25 +0200

    Remove hardcoded secrets

--- get_spotify_token.py
+++ get_spotify_token.py
@@ -24,8 +24,8 @@ from urllib.parse import urlparse, parse_qs, urlencode
 # --- STEP 1: PASTE YOUR SPOTIFY APP CREDENTIALS HERE ---
 # You can get these from your Spotify Developer Dashboard:
 # https://developer.spotify.com/dashboard/
-CLIENT_ID = "7270af283ac647ed8ba230b5826f7d1b"
-CLIENT_SECRET = "208eae8bce8b43c1b7a6ea0766d6151e"
+CLIENT_ID = "your_spotify_client_id"
+CLIENT_SECRET = "your_spotify_client_secret"
 
 # --- Configuration (usually no need to change) ---
 PORT = 8888
